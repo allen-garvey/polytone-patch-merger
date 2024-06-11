d6135f9bb0051075da560e2a8e85de2d42677120
Allen Garvey <allengarvey@hotmail.com>
2024-06-11 17:45:32 -0400

    Add file inputs and ability to read files as strings

--- js/components/app.vue
+++ js/components/app.vue
@@ -1,6 +1,34 @@
 <template>
+    <div class="alert alert-danger" v-if="errorMessage">{{ errorMessage }}</div>
     <div :class="$style.container">
-        Hello
+        <div :class="$style.fileInputsContainer">
+            <div :class="[$style.inputBox, $style.inputBoxBlue]">
+                <file-input 
+                    title="Patch 1" 
+                    id="patch1-file-input" 
+                    :onError="onError"
+                    :onFileSelected="(file) => onFileSelected('patch1', file)"
+                    v-if="patch1 === null" 
+                />
+                <div :class="$style.inputPatchName" v-else>
+                    <h3>{{ patch1.name }}</h3>
+                    <button class="btn btn-sm btn-light" @click="patch1 = null">Clear</button>
+                </div>
+            </div>
+            <div :class="[$style.inputBox, $style.inputBoxRed]">
+                <file-input 
+                    title="Patch 2" 
+                    id="patch2-file-input" 
+                    :onError="onError"
+                    :onFileSelected="(file) => onFileSelected('patch2', file)"
+                    v-if="patch2 === null" 
+                />
+                <div :class="$style.inputPatchName" v-else>
+                    <h3>{{ patch2.name }}</h3>
+                    <button class="btn btn-sm btn-light" @click="patch2 = null">Clear</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,16 +36,45 @@
     .container {
 
     }
+    .fileInputsContainer {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+    }
+    .inputBox {
+        flex-grow: 1;
+        color: #fafafa;
+        padding: 4rem 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+    .inputBoxBlue {
+        background-color: #1c5387;
+    }
+    .inputBoxRed {
+        background-color: #9c281d;
+    }
+    .inputPatchName {
+        text-align: center;
+    }
 </style>
 
 <script>
+import fileInput from './fileInput.vue';
+
 export default {
     components: {
+        fileInput,
     },
     created(){
     },
     data(){
         return {
+            errorMessage: '',
+            patch1: null,
+            patch2: null,
         };
     },
     computed: {
@@ -25,6 +82,14 @@ export default {
     watch: {
     },
     methods: {
+        onFileSelected(key, file){
+            console.log(file);
+            this.errorMessage = '';
+            this[key] = file;
+        },
+        onError(message){
+            this.errorMessage = message;
+        },
     }
 };
 </script>
---
+++ js/components/fileInput.vue
@@ -0,0 +1,88 @@
+<template>
+    <div :class="$style.container">
+        <label :for="id" :class="$style.label" class="form-label">{{ title }}</label>
+        <input type="file" :id="id" class="form-control" @change="fileSelected" />
+    </div>
+</template>
+
+<style lang="scss" module>
+    .container {
+
+    }
+    .label {
+        display: inline-block;
+        margin-bottom: 0.2em;
+    }
+</style>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        id: {
+            type: String,
+            required: true,
+        },
+        onError: {
+            type: Function,
+            required: true,
+        },
+        onFileSelected: {
+            type: Function,
+            required: true,
+        },
+    },
+    created(){
+        this.fileReader = new FileReader();
+        this.fileReader.onload = (e) => {
+            if(e.target.readyState != 2) {
+                return;
+            }
+            if(e.target.error) {
+                return this.onError(`There was a problem reading ${this.fileName}.`);
+            }
+
+            this.fileRead(e.target.result);
+        };
+    },
+    data(){
+        return {
+            fileReader: null,
+            fileName: '',
+        };
+    },
+    computed: {
+    },
+    watch: {
+    },
+    methods: {
+        fileSelected($event){
+            const files = $event.target.files;
+            
+            if(files.length === 0){
+                return this.onError('No files selected.');
+            }
+            if(files.length > 1){
+                return this.onError('More than 1 file selected.');
+            }
+            const file = files[0];
+            if(!/\.repatch$/.test(file.name)){
+                return this.onError(`${file.name} does not appear to be a Polytone patch.`);
+            }
+            this.fileName = file.name;
+            this.fileReader.readAsText(file);
+        },
+        fileRead(content){
+            // TODO validate valid XML and possibly DTD first
+            // also check xml to see if this is actually a polytone patch
+            this.onFileSelected({
+                name: this.fileName,
+                content,
+            });
+        },
+    }
+};
+</script>

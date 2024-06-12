6a691a71b2931015a2c59c14ef35bd47608d6edf
Allen Garvey <allengarvey@hotmail.com>
2024-06-12 10:44:47 -0400

    Refactor patch inputs into separate components

--- js/components/app.vue
+++ js/components/app.vue
@@ -2,32 +2,24 @@
     <div class="alert alert-danger" v-if="errorMessage">{{ errorMessage }}</div>
     <div :class="$style.container">
         <div :class="$style.fileInputsContainer">
-            <div :class="[$style.inputBox, $style.inputBoxBlue]">
-                <file-input 
-                    title="Patch 1" 
-                    id="patch1-file-input" 
-                    :onError="onError"
-                    :onFileSelected="(file) => onFileSelected('patch1', file)"
-                    v-if="patch1 === null" 
-                />
-                <div :class="$style.inputPatchName" v-else>
-                    <h3>{{ patch1.name }}</h3>
-                    <button class="btn btn-sm btn-light" @click="patch1 = null">Clear</button>
-                </div>
-            </div>
-            <div :class="[$style.inputBox, $style.inputBoxRed]">
-                <file-input 
-                    title="Patch 2" 
-                    id="patch2-file-input" 
-                    :onError="onError"
-                    :onFileSelected="(file) => onFileSelected('patch2', file)"
-                    v-if="patch2 === null" 
-                />
-                <div :class="$style.inputPatchName" v-else>
-                    <h3>{{ patch2.name }}</h3>
-                    <button class="btn btn-sm btn-light" @click="patch2 = null">Clear</button>
-                </div>
-            </div>
+            <patch-input 
+                title="Patch 1" 
+                id="patch1-file-input" 
+                :containerClassName="$style.inputBoxBlue"
+                :onError="onError"
+                :onFileSelected="(file) => onFileSelected('patch1', file)"
+                :onPatchCleared="() => patch1 = null"
+                :patch="patch1"
+            />
+            <patch-input 
+                title="Patch 2" 
+                id="patch2-file-input" 
+                :containerClassName="$style.inputBoxRed"
+                :onError="onError"
+                :onFileSelected="(file) => onFileSelected('patch2', file)"
+                :onPatchCleared="() => patch2 = null"
+                :patch="patch2"
+            />
         </div>
     </div>
 </template>
@@ -41,32 +33,20 @@
         flex-wrap: wrap;
         gap: 1rem;
     }
-    .inputBox {
-        flex-grow: 1;
-        color: #fafafa;
-        padding: 4rem 2rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-wrap: wrap;
-    }
     .inputBoxBlue {
         background-color: #1c5387;
     }
     .inputBoxRed {
         background-color: #9c281d;
     }
-    .inputPatchName {
-        text-align: center;
-    }
 </style>
 
 <script>
-import fileInput from './fileInput.vue';
+import patchInput from './patchInput.vue';
 
 export default {
     components: {
-        fileInput,
+        patchInput,
     },
     created(){
     },
---
+++ js/components/patchInput.vue
@@ -0,0 +1,68 @@
+<template>
+    <div :class="[$style.container, containerClassName]">
+        <file-input 
+            :title="title" 
+            :id="id" 
+            :onError="onError"
+            :onFileSelected="onFileSelected"
+            v-if="patch === null" 
+        />
+        <div :class="$style.inputPatchName" v-else>
+            <h3>{{ patch.name }}</h3>
+            <button class="btn btn-sm btn-light" @click="onPatchCleared">Clear</button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" module>
+    .container {
+        flex-grow: 1;
+        color: #fafafa;
+        padding: 4rem 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+    .inputPatchName {
+        text-align: center;
+    }
+</style>
+
+<script>
+import fileInput from './fileInput.vue';
+
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        containerClassName: {
+            type: String,
+        },
+        id: {
+            type: String,
+            required: true,
+        },
+        onError: {
+            type: Function,
+            required: true,
+        },
+        onFileSelected: {
+            type: Function,
+            required: true,
+        },
+        onPatchCleared: {
+            type: Function,
+            required: true,
+        },
+        patch: {
+            type: Object,
+        },
+    },
+    components: {
+        fileInput,
+    },
+};
+</script>

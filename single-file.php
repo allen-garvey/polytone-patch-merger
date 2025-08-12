7ff998f25e7e18f4a944c24b0b3b4a1445f57611
Allen Garvey <allen.garvey@gmail.com>
2025-08-12 11:47:03 -0400

    Add link to download single file version

--- single-file.php
+++ single-file.php
@@ -6,4 +6,4 @@ $index = file_get_contents($current_directory.'/public/index.html');
 $js = file_get_contents($current_directory.'/public/assets/app.js');
 $css = file_get_contents($current_directory.'/public/assets/style.css');
 
-echo str_replace('<script src="/assets/app.js"></script>', '<script>'.$js.'</script>', str_replace('<link rel="stylesheet" type="text/css" href="/assets/style.css"/>', '<style>'.$css.'</style>', $index));
+echo str_replace('<script src="/assets/app.js"></script>', '<script>'.$js.'</script>', str_replace('<link rel="stylesheet" type="text/css" href="/assets/style.css" />', '<style>'.$css.'</style>', preg_replace('|<a href="/polytone-patch-merger\\.html" download[^<]+</a\\s*>|', '', $index)));
